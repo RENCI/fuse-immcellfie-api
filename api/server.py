7aa6787377f44bed44ec075d4d51afef95bb7071
luthfib <luthfibustillos@gmail.com>
2021-03-05 14:42:06 -0500

    remove endpoint and change endpoint location

--- api/server.py
+++ api/server.py
@@ -4,7 +4,7 @@ from tx.connexion.utils import ReverseProxied
 
 def create_app():
     app = connexion.FlaskApp(__name__, specification_dir="openapi/")
-    app.add_api("my_api.yaml")
+    app.add_api("api.yaml")
     flask_app = app.app
     proxied = ReverseProxied(flask_app.wsgi_app)
     flask_app.wsgi_app = proxied

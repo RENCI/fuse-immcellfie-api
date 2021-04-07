ce68aaafd7817be16a671ac276b6af3dfd62eb08
luthfib <luthfibustillos@gmail.com>
2021-04-07 14:33:03 -0400

    hit analysis engine for file instead

--- api/routes.py
+++ api/routes.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Response, jsonify, request
 from flask_cors import CORS, cross_origin
+import requests
 
 from api.utils.file import get_file, root_dir
 
@@ -12,17 +13,23 @@ def hello_world():
 
 @cross_origin()
 def serve_file():
+    analysis_url = "http://localhost:8080/v1/plugin/fuse-analysis-cellfie/analysis"
     if filename := request.args.get("filename"):
-        try:
-            content = get_file(filename)
-            return Response(content, mimetype="text/html")
-        except Exception:
-            return jsonify(
-                {
-                    "These are the following files available": os.listdir(
-                        "/usr/src/app/data"
-                    )
-                }
-            )
+        response = requests.get(f"{analysis_url}?filename={filename}")
+        return Response(response.text, mimetype="text/html")
     else:
-        return "Please provide the filename query param ", 404
+        return "Please provide the filename query param ", 400
+    # if filename := request.args.get("filename"):
+    #     try:
+    #         content = get_file(filename)
+    #         return Response(content, mimetype="text/html")
+    #     except Exception:
+    #         return jsonify(
+    #             {
+    #                 "These are the following files available": os.listdir(
+    #                     "/usr/src/app/data"
+    #                 )
+    #             }
+    #         )
+    # else:
+    #     return "Please provide the filename query param ", 404
